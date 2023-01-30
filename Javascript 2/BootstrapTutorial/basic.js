fe287cf1c6076c15ff224e2913231f51e573f9ce
Scott Winiarski <Scott1w1@hotmail.com>
2023-01-30 16:18:32 -0600

    Brought in material for the second lab for javascript.

--- Javascript 2/BootstrapTutorial/basic.js
+++ Javascript 2/BootstrapTutorial/basic.js
@@ -4,11 +4,8 @@ let n = [20, 54, 82];
 const x = 5;
 const y = 2;
 
-function isDivisible(n, x, y){
-    if(n / x === 0){
-        $('.mathOutput').text(n + " is divisible");
-    }
-    else if(n % y === 0){
+function isDivisible(n){
+    if(n % y === 0){
         $('.mathOutput').text(n + " is divisible");
     }
     else{
