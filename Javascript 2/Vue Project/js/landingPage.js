b8c41dd1dcbd010f6334828d272f10c730364885
ScottWiniarski <Scott1W1@hotmail.com>
2023-03-26 21:02:06 -0500

    Added some flair for the index page

---
+++ Javascript 2/Vue Project/js/landingPage.js
@@ -0,0 +1,18 @@
+app.component('landingPage', {
+
+    template: `
+      <div class="container-md">
+      <h2 class="text-center">A message from the Company</h2>
+        <div class="container">
+          <p class="text-center">
+            <h4> Welcome Miners!</h4>
+            
+            Unfortunately, due to the recent increase in hostilities with local wildlife, management
+            has determined that all employees must have a B.A.C percentage of less than .05 before deployment 
+            to a work-zone. Management appreciates your compliance, and reminds you that failure to meet this 
+            standard comes with penalties to your employee record.
+          </p>
+        </div>
+      </div>
+    `
+});
