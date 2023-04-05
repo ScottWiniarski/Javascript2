d91f563187383d08189d7b2be379eb4ebe8bcfa5
Scott Winiarski <Scott1w1@hotmail.com>
2023-04-05 15:35:52 -0500

    Added and finished collections example for OOP

---
+++ Javascript 2/Start-it-demo/vue-components-collections/js/app.js
@@ -0,0 +1,21 @@
+const app = Vue.createApp({
+    // data: all the data for the app
+    data(){
+        return {}
+    },
+
+    // methods: usually "events" triggered by v-on:
+    methods: {
+
+    },
+    // computed: values that are updated and cached if dependencies change
+    computed: {
+
+    },
+
+    //mounted:  called after the instance has been mounted,
+    mounted: function() {
+
+    },
+
+});
